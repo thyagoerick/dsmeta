41f0afecdfa28f309bf864a2324b3cc913cb8621
Thyago Erick <thyago123gois@gmail.com>
2023-02-02 02:05:38 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Olá mundo!</h1>
+    //O Fragment (<> </>) serve para permitir que o componente react exporte mais de uma tag
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton/>
+    </>
   )
 }
 
