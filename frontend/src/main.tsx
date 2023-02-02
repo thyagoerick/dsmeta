bd2913ac714e93774c53cb7dd98b52233e262a1e
Thyago Erick <thyago123gois@gmail.com>
2023-02-02 01:42:44 -0300

    Project clean

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+
+//O processo de converter informações em imagem/visual, chama-se renderização
+
+//                                                                   /-> o .render() manda renderizar a tela no navegador
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
