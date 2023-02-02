6f11d00b9e20b669c7611d374e80036612d2a1df
Thyago Erick <thyago123gois@gmail.com>
2023-02-02 02:34:51 -0300

    Others components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
   return (
     //O Fragment (<> </>) serve para permitir que o componente react exporte mais de uma tag
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton/>
+      <Header/>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+import './style.css'
+
+function Header() {
+    return (
+        //O Fragment (<> </>) serve para permitir que o componente react exporte mais de uma tag
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/7.3.5.6/">@7.3.5.6</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
