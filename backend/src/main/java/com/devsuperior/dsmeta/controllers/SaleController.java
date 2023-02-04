9f8feabb56a60a91deb71e5816af628ef35943a9
Thyago Erick <thyago123gois@gmail.com>
2023-02-04 04:35:50 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 
 //o cotroller vira uma api com a anotation de rest
@@ -26,6 +28,8 @@ public class SaleController {
 	
 	@Autowired //serve para injetar uma instancia da classe que está embaixo dele
 	private SaleService service;
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping // para o método abaixo responder usando o verbo get do http
 	public Page<Sale> findSales(
@@ -44,5 +48,10 @@ public class SaleController {
 		 * que assume a paginação do objeto da lista quando passado por parâmetro
 		 */
 	}
-
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+		
+	}
 }
