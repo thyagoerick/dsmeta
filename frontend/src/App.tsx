e302858eb434ca4ba8d8efaac9e9b66d0ae0071e
Thyago Erick <thyago123gois@gmail.com>
2023-02-05 01:25:19 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     //O Fragment (<> </>) serve para permitir que o componente react exporte mais de uma tag
     <>
-      <Header/>
+      <ToastContainer />
+      <Header />
       <main>
         <section id="sales">
           <div className="dsmeta-container">
-            <SalesCard/>
+            <SalesCard />
           </div>
         </section>
       </main>
